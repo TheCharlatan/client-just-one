f277fab1806e3d6e53fc6529372f78107023cea3
RaffaelCH <55450318+RaffaelCH@users.noreply.github.com>
2020-04-12 14:33:20 +0200

    FS20G06-99: Added layout for mystery word (No positioning yet).

--- src/components/game/wordSelection/SelectNumberContainer.js
+++ src/components/game/wordSelection/SelectNumberContainer.js
@@ -10,18 +10,17 @@ import Blue from "../../../views/design/font-families/Blue";
 import { api } from '../../../helpers/api';
 
 
-
 // <SelectNumberContainer gameId={this.state.gameModel.id}/>
 
 export function SelectNumberContainer(props) {
 
     return (
         <ButtonBox>
-            <NumberButton id={this.props.gameId} number={1} color={"red"} />
-            <NumberButton id={this.props.gameId} number={2} color={"orange"} />
-            <NumberButton id={this.props.gameId} number={3} color={"green"} />
-            <NumberButton id={this.props.gameId} number={4} color={"blue"} />
-            <NumberButton id={this.props.gameId} number={5} color={"red"} />
+            <NumberButton id={props.gameId} number={1} color={"red"} />
+            <NumberButton id={props.gameId} number={2} color={"orange"} />
+            <NumberButton id={props.gameId} number={3} color={"green"} />
+            <NumberButton id={props.gameId} number={4} color={"blue"} />
+            <NumberButton id={props.gameId} number={5} color={"red"} />
         </ButtonBox>
     );
 }
@@ -70,6 +69,7 @@ function NumberButton(props) {
 }
 
 
+// TODO: Position it properly.
 let ButtonBox = styled.div`
 padding: 0.5em;
 border-radius: 2px;
@@ -79,7 +79,7 @@ display: flex;
 flex-wrap: wrap;
 
 background: #F8E7D1;
-border: 4px solid #DDC18E;
+border: 3px solid #DDC18E;
 box-sizing: border-box;
 box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `;
@@ -89,5 +89,5 @@ let ButtonWhite = styled(Button)`
 margin: 0.3em;
 width: 12em;
 background: #FFFFFF;
-border: 4px solid #DDC18E;
+border: 3px solid #DDC18E;
 `;
