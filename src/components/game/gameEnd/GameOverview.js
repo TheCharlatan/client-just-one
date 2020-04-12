4cd46dc56503632f3467af1e72b0dc0de0062c01
RaffaelCH <55450318+RaffaelCH@users.noreply.github.com>
2020-04-12 16:44:58 +0200

    Added basic directory structure and classes to give an idea.

---
+++ src/components/game/gameEnd/GameOverview.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+// The end of game overview.
+class GameOverview extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+          <p>Placeholder</p>
+        );
+    }
+}
