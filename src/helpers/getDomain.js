225f9ca6b199840c024e6fbb5d47d978a20b152c
RaffaelCH <55450318+RaffaelCH@users.noreply.github.com>
2020-05-02 20:19:56 +0200

    Fixed dev url.

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -8,7 +8,7 @@ import { isProduction } from './isProduction';
  */
 export const getDomain = () => {
   const prodUrl = 'https://sopra-fs20-group-06-server.herokuapp.com/'; // TODO: insert your groups heroku prod url for server (once deployed)
-  const devUrl = 'http://192.168.0.24:8080';
+  const devUrl = 'http://localhost:8080';
 
   return isProduction() ? prodUrl : devUrl;
 };
