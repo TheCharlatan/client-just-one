62f62d07a76fb1a0ab2bd17f8ae1c256f9126d04
RaffaelCH <55450318+RaffaelCH@users.noreply.github.com>
2020-05-18 17:16:56 +0200

    FS20G06-263 - Update leaderboard periodically.

--- src/components/mainpage/leaderboard/Leaderboard.js
+++ src/components/mainpage/leaderboard/Leaderboard.js
@@ -13,52 +13,30 @@ class Leaderboard extends React.Component {
         super(props);
         this.state = {
             leaderboardUsers: [],
-            orderedDescending: true
+            orderedDescending: true,
+            updateTimer: null
         };
+        this.updateLeaderboard = this.updateLeaderboard.bind(this);
         this.orderLeaderboard = this.orderLeaderboard.bind(this);
         this.scrollLeaderboardToLeader = this.scrollLeaderboardToLeader.bind(this);
         this.scrollLeaderboardToCurrentUser = this.scrollLeaderboardToCurrentUser.bind(this);
-        /*
-        // test data
-        this.setState({
-            leaderboardUsers: [
-                {id: 1, username: 'user1', score: 1},
-                {id: 2, username: 'user2', score: 2},
-                {id: 3, username: 'user3', score: 3},
-                {id: 4, username: 'user4', score: 4},
-                {id: 5, username: 'user5', score: 5},
-                {id: 6, username: 'user6', score: 6},
-                {id: 7, username: 'user7', score: 7},
-                {id: 8, username: 'user8', score: 8},
-                {id: 9, username: 'user9', score: 9},
-                {id: 10, username: 'user10', score: 10},
-                {id: 11, username: 'user11', score: 11},
-                {id: 12, username: 'user12', score: 12},
-                {id: 13, username: 'user13', score: 13},
-                {id: 14, username: 'user14', score: 14},
-                {id: 15, username: 'user15', score: 15},
-                {id: 16, username: 'user16', score: 16},
-                {id: 17, username: 'user17', score: 17},
-                {id: 18, username: 'user18', score: 18},
-                {id: 19, username: 'user19', score: 19},
-                {id: 20, username: 'user20', score: 20},
-                {id: 21, username: 'user21', score: 21},
-                {id: 22, username: 'user22', score: 22},
-                {id: 23, username: 'user23', score: 23},
-                {id: 24, username: 'user24', score: 24},
-                {id: 25, username: 'user25', score: 25},
-                {id: 26, username: 'user26', score: 26},
-                {id: 27, username: 'user27', score: 27},
-                {id: 28, username: 'user28', score: 28},
-                {id: 29, username: 'user29', score: 29}
-            ]
-        });
-         */
     }
 
 
     async componentDidMount() {
+        await this.updateLeaderboard();
+        let timer = setInterval(this.updateLeaderboard, 2000);
+        this.setState({updateTimer: timer});
+    }
 
+
+    componentWillUnmount() {
+        clearInterval(this.state.updateTimer);
+        this.setState({updateTimer: null});
+    }
+
+
+    async updateLeaderboard() {
         try {
             let requestHeader = 'X-Auth-Token ' + sessionStorage.getItem('token');
             let response = await api.get(`/user/scoreboard`, {headers: {'X-Auth-Token': requestHeader}});
@@ -68,7 +46,6 @@ class Leaderboard extends React.Component {
         }
         catch (error) {
             alert(`An error occurred when retrieving leaderboard data: ${handleError(error)}`);
-            return;
         }
     }
 
